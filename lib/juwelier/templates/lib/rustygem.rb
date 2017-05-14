f912e5c431e4470b062d25f424ed6c30ea8cc163
Fred Mitchell <fred.mitchell@gmx.de>
2017-05-14 06:53:29 +0200

    Rusty Gem example now works and illustrats how to safely pass strings into Rust

--- lib/juwelier/templates/lib/rustygem.rb
+++ lib/juwelier/templates/lib/rustygem.rb
@@ -4,8 +4,8 @@ module <%= constant_name %>
   extend FFI::Library
   ffi_lib 'rust/target/release/<%= extension_name %>'
   
-  attach_function :simple_test, [ :string, :int ], :int
-  attach_function :hello_world, [ :string, :int ], :int
+  attach_function :simple_test, [ :string, :uint ], :uint
+  attach_function :hello_world, [ :string, :uint ], :uint
 end
 
 if $0 == __FILE__
@@ -13,6 +13,6 @@ if $0 == __FILE__
   {"hello":"Hello", "world":"World" }
 JSON
  
-  puts <%= constant_name %>::simple_test "Hi there!", 10
+  puts <%= constant_name %>::simple_test "Hi there!", 21
   puts <%= constant_name %>::hello_world json, 3
 end
